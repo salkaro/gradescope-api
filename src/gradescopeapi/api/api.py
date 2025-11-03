b32590481e4676fe6f49e6be23922f94293c1571
RoyalGr4pe <njames.programming@gmail.com>
2025-11-03 15:04:40 +0000

    add origin

--- src/gradescopeapi/api/api.py
+++ src/gradescopeapi/api/api.py
@@ -16,6 +16,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:3000",
         "https://unihub.salkaro.com",
+        "https://unihubsalkaro.netlify.app/",
     ],
     allow_credentials=True,
     allow_methods=["*"],
